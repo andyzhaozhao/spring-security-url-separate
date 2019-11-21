63323da4f7032c91e70184b0f8a38907b7e17543
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2019-11-21 11:45:54 +0800

    Add:init project

--- spring-security-url-separate-sample/src/main/java/com/hoioy/springsecurityurlseparate/config/GroupTwoSecurityConfig.java
+++ spring-security-url-separate-sample/src/main/java/com/hoioy/springsecurityurlseparate/config/GroupTwoSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.hoioy.springsecurityurlseparate.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +14,7 @@ public class GroupTwoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/error","/login**").permitAll()
+                .antMatchers("/error", "/login**").permitAll()
                 .anyRequest().fullyAuthenticated()
                 .and().formLogin()
                 .and().oauth2Login();
